c3f77c631cfd68d8148931acc14a4e37563a4968
nxhuel <nahueltisera03@gmail.com>
2024-08-18 18:33:11 -0300

    Adding Movies.java in models

--- src/main/java/com.nxhuel.movies.models/Movie.java
+++ src/main/java/com.nxhuel.movies.models/Movie.java
@@ -13,6 +13,62 @@ public class Movie {
     private int year;
     private int votes;
     private double rating;
-    @Column(name = "image_url")
+    @Column(name = "image_url" , length = 255)
     private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
